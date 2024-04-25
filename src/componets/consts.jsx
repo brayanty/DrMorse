216ec73e814699050a93d01e436fe9ddf44dcf80
Brayan <danielsegoviad@gmail.com>
2024-04-25 10:49:08 -0500

    Delete componet Hero

--- src/componets/consts.jsx
+++ src/componets/consts.jsx
@@ -1,23 +1,24 @@
-export const headers = [
+export const heros = [
   {
-    title: "Bienvenido a Dr Morse",
-    paragraph: "Un Laboratorio Especializado en Odontología con más de 11 años de experiencia.",
-    src: "src/Img/pexels-anna-shvets-3845682.jpg",
-    alt: "Imagenes de especialistas medicos y un paciente",
-    select: "0",
-  },
-  {
-    title: '¿Necesitas ayuda con tus dientes?',
-    paragraph: 'En Dr Morse estemos encantados de ayudarte.',
-    src: 'src/Img/pexels-cedric-fauntleroy-4269689.jpg',
-    alt: 'Imagen de un odontologo con unos dientes artificiales',
-    select: "1",
-  },
-  {
-    title: 'Bienvenido a Dentitc',
-    paragraph: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit dignissimos perspiciatis officiis blanditiis.',
-    src: 'src/Img/instrumental-odontologico.jpg',
-    alt: 'Imagenes de un instrumentos odontologíco',
-    select: "2",
+    "title": "Bienvenido a Dr. Mose",
+    "paragraph": "Dentitc ofrece servicios odontológicos de primera calidad, centrados en la comodidad y satisfacción de nuestros pacientes. Nuestro compromiso es proporcionar tratamientos personalizados para mantener tu salud bucal en óptimas condiciones.",
+    "src": "src/Img/instrumental-odontologico.jpg",
+    "alt": "Imagen de instrumentos odontológicos utilizados en nuestras instalaciones",
+    "select": "2"
   }
+    {
+    "title": "Estamos comprometidos con las salud de tus dientes",
+    "paragraph": "Dr. Morse es un laboratorio especializado en odontología con más de 11 años de experiencia, comprometido con la salud bucal y el bienestar de nuestros pacientes.",
+    "src": "src/Img/pexels-anna-shvets-3845682.jpg",
+    "alt": "Imagen de especialistas médicos y un paciente sonriendo",
+    "select": "0"
+  },
+  {
+    "title": "¿Necesitas ayuda con tus dientes?",
+    "paragraph": "En Dr. Morse, nuestro equipo de profesionales altamente capacitados está aquí para brindarte la mejor atención odontológica. ¡Déjanos cuidar de tu sonrisa!",
+    "src": "src/Img/pexels-cedric-fauntleroy-4269689.jpg",
+    "alt": "Imagen de un odontólogo examinando unos dientes artificiales",
+    "select": "1"
+  },
+
 ];
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,14 @@ import "./css/index.css";
 import "./css/normalize.css";
 
 import Navbar from "./componets/Nav.jsx"
-import Hero from "./componets/Hero.jsx";
 import MainSection from "./componets/mainSection.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <header className="Header">
+    <header className="Header ">
       <Navbar />
     </header>
-    <Hero />
+    
     <MainSection />
   </React.StrictMode>
 );
