a11e964237da52fdade7c767d748a060ed9a7725
Brayan <danielsegoviad@gmail.com>
2024-04-30 22:18:32 -0500

    Add component Main

---
+++ src/componets/Main.jsx
@@ -0,0 +1,36 @@
+
+function Main() {
+    return (
+    <main className="main">
+        <section className="main_container">
+            <article className="main_whats">
+                <figure className="main_whats-img">
+                    <img src="./Img" alt="" />
+                </figure>
+                <div className="main_whats-text">
+                    <h4 className="title-2">¿Sabias que?</h4>
+                    <p className="paragraph-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quaerat, eveniet perferendis omnis quibusdam a esse molestiae autem magni repellendus soluta fuga vitae deleniti error officiis natus voluptatum voluptates dolorem!</p>
+                </div>
+            </article>
+        </section>
+        <aside className="section-aside">
+            <nav className="aside_social-nav">
+                <ul className="social-nav">
+                    <li className="nav-items">
+                        <a className="nav-item" href="#">facebook</a>
+                    </li>
+                    <li className="nav-items">
+                        <a className="nav-item" href="#">Instagram</a>
+                    </li>
+                    <li className="nav-items">
+                        <a className="nav-item" href="#">Twiter</a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    </main>
+    )
+}
+
+
+export default Main;
